07fa14caf2c19f37cbfd0869c9495c5261947372
Eduardo Garcia Zaccharias <71724634+eduardogarciazaccharias@users.noreply.github.com>
2020-11-20 08:00:57 -0300

    Create voxel_clube.h

---
+++ voxel_clube.h
@@ -0,0 +1,10 @@
+#ifndef VOXEL_CLUBE_H_INCLUDED
+#define VOXEL_CLUBE_H_INCLUDED
+
+struct Voxel {
+  float r,g,b; // Cores
+  float a;    // Transparencia
+  bool isOn; // Inclui ou não
+};
+
+#endif // VOXEL_CLUBE_H_INCLUDED
